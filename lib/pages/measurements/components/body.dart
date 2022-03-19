d8ec5dd587c72b5d98db799adf2a29c79df5cd49
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-03-20 01:55:22 +0200

    Update body.dart

--- lib/pages/measurements/components/body.dart
+++ lib/pages/measurements/components/body.dart
@@ -5,10 +5,23 @@ import 'package:flutter/material.dart';
 class Body extends StatelessWidget {
   const Body({ Key? key }) : super(key: key);
 
+  // Widget loading() {
+  //   return Scaffold(
+  //     body: Padding(
+  //       padding: const EdgeInsets.all(20.0),
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //         children: <Widget>[
+  //           Center(
+  //           child: CircularProgressIndicator(),
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
