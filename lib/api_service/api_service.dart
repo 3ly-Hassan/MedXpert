773590a84b4e4fe73f7fbac48c90a523bdc730c5
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-03-20 01:25:16 +0200

    factor

--- lib/api_service/api_service.dart
+++ lib/api_service/api_service.dart
@@ -1,5 +1,4 @@
-import 'package:final_pro/cache_helper.dart';
-import 'package:final_pro/models/measurement.dart';
+import 'package:final_pro/constants.dart';
 import 'package:final_pro/models/signup_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -7,11 +6,16 @@ import 'dart:convert';
 import '../models/login_model.dart';
 
 class APIService {
-  final token = CacheHelper.getData(key: "token");
+  String api = "http://10.0.2.2:8000/api";
 
+   final Map<String, String> headers =  {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
 
   Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
-    String url = "http://10.0.2.2:8000/api/auth/login";
+    String url = "$api/auth/login";
 
     final response =
         await http.post(Uri.parse(url), body: requestModel.toJson());
@@ -21,7 +25,7 @@ class APIService {
   }
 
   Future<SignUpResponseModel> signUp(SignUpRequestModel requestModel) async {
-    String url = "http://10.0.2.2:8000/api/auth/register";
+    String url = "$api/auth/register";
 
     final response =
         await http.post(Uri.parse(url), body: requestModel.toJson());
@@ -33,15 +37,11 @@ class APIService {
 
 
   Future<List<dynamic>> get_measurements() async {
-    String url = "http://10.0.2.2:8000/api/vitalSign/getvitalSign";
+    String url = "$api/vitalSign/getvitalSign";
     print("getting data");
 try {
    final response =
-        await http.get(Uri.parse(url),headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': 'Bearer $token',
-    });  
+        await http.get(Uri.parse(url),headers: headers );  
   if(response.statusCode == 200){
     final List<dynamic> measurement = json.decode(response.body)["data"];
     return measurement;
--- lib/pages/measurements/measurements.dart
+++ lib/pages/measurements/measurements.dart
@@ -2,6 +2,7 @@ import 'package:final_pro/cubits/MeasuremetCubit/measurement_cubit.dart';
 import 'package:final_pro/models/measurement.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'components/body.dart';
 
 class Measurements extends StatefulWidget {
   const Measurements({Key? key}) : super(key: key);
@@ -26,15 +27,7 @@ class _MeasurementsState extends State<Measurements> {
       appBar: AppBar(
         title: Text("Measurements"),
       ),
-      body: BlocBuilder<MeasurementCubit, MeasurementState>(
-          builder: (context, state) {
-        if (state is MeasurementLoaded) {
-          all_measurements =MeasurementCubit.get(context).measurements;
-          return Text(all_measurements[0].condition);
-        } else {
-          return Text("not recieved");
-        }
-      }),
+      body: Body(),
     );
   }
 }
