dfec7ea4d0d675b031bccc0c8dc8450b85cab3a4
Azab007 <57720086+Azab007@users.noreply.github.com>
2022-03-22 19:03:27 +0200

    Update medication.dart

--- lib/models/medication.dart
+++ lib/models/medication.dart
@@ -3,12 +3,14 @@ class Medication {
   String? patientId;
   String? doctorId;
   bool? currentlyTaken;
+  List<dynamic>? drugs;
   String? createdAt;
   String? updatedAt;
 
   Medication({
     this.id,
     this.patientId,
+    this.drugs,
     this.doctorId,
     this.currentlyTaken,
     this.createdAt,
@@ -19,6 +21,7 @@ class Medication {
     id = json['_id'];
     patientId = json['patient_id'];
     doctorId = json['doctor_id'];
+    drugs = json["drugs"];
     currentlyTaken = json['currentlyTaken'];
     createdAt = json['createdAt'];
     updatedAt = json['updatedAt'];
