6ad34f89f33ad90819b1d851ca048ccd6da24195
Ali Hassan Ali <ah074321@gmail.com>
2022-05-16 22:50:29 +0200

    scaffold error

--- lib/pages/logging_page/not verified.dart
+++ lib/pages/logging_page/not verified.dart
@@ -8,45 +8,47 @@ class NotVerified extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            LineAwesomeIcons.mail_bulk,
-            size: 200,
-            color: Colors.black26,
-          ),
-          Text(
-            'you need to verify your email ',
-            style: TextStyle(fontSize: 24),
-          ),
-          Text(
-            'Check your mail inbox',
-            style: TextStyle(fontSize: 24),
-          ),
-          SizedBox(
-            height: 60,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, LoggingPage.routeName);
-            },
-            style: ElevatedButton.styleFrom(
-              primary: Colors.greenAccent,
-              fixedSize: const Size(300, 70),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50)),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              LineAwesomeIcons.mail_bulk,
+              size: 200,
+              color: Colors.black26,
             ),
-            child: Text(
-              'Log In',
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w700),
+            Text(
+              'you need to verify your email ',
+              style: TextStyle(fontSize: 24),
             ),
-          )
-        ],
+            Text(
+              'Check your mail inbox',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, LoggingPage.routeName);
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.greenAccent,
+                fixedSize: const Size(300, 70),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+              ),
+              child: Text(
+                'Log In',
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w700),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
