a4d2791b149c349a6654c9d7b6b56df891b91724
Abdo Nabil <abdo.sw.eng@gmail.com>
2022-07-15 17:40:01 +0200

    some changes in UI 4

--- lib/theme.dart
+++ lib/theme.dart
@@ -74,10 +74,10 @@ TextTheme textTheme() {
 
 AppBarTheme appBarTheme() {
   return AppBarTheme(
-    color: Colors.black,
+    color: Colors.white,
     elevation: 0,
     brightness: Brightness.light,
-    iconTheme: IconThemeData(color: Colors.white),
+    iconTheme: IconThemeData(color: Colors.black),
     textTheme: TextTheme(
       headline6: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18),
     ),
